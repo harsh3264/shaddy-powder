f5ce1b24cbefcdb113757d0b7037de57701d83dc
harsh3264 <harshraval3264@gmail.com>
2024-10-05 13:30:30 +0000

    Minor CSV changes

--- db_updates/live_update_fixture_tables.py
+++ db_updates/live_update_fixture_tables.py
@@ -49,24 +49,7 @@ from database.live_fixture_player_stats import load_fixture_player_stats
 query = '''
     SELECT DISTINCT fixture_id
     FROM live_updates.live_fixtures f
-    WHERE
-        (
-            DAYOFWEEK(FROM_UNIXTIME(f.timestamp)) = 7  -- Saturday
-            AND f.timestamp = 1727532000  -- 3 PM kickoff for PL matches
-            AND f.league_id = 39  -- Only focus on Premier League matches
-        )
-        OR
-        (
-            -- For other days, or if it is Saturday but outside the restricted time
-            DAYOFWEEK(FROM_UNIXTIME(f.timestamp)) != 7  -- For other days, no restrictions
-            OR
-            (
-                DAYOFWEEK(FROM_UNIXTIME(f.timestamp)) = 7
-                AND (
-                    TIME(FROM_UNIXTIME(f.timestamp)) NOT BETWEEN '13:20:00' AND '14:10:00'  -- Exclude other leagues in this time
-                )
-            )
-        )
+    # WHERE league_id = 39
     ;
 '''
     
