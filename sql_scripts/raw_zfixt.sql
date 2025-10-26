cc7e201658d90d0c0afde4a12e042ebf2e5d1374
harsh3264 <harshraval3264@gmail.com>
2025-10-26 20:15:07 +0000

    Live Ref Fix v2

--- sql_scripts/raw_zfixt.sql
+++ sql_scripts/raw_zfixt.sql
@@ -7,12 +7,11 @@
 -- FROM today_fixture
 -- WHERE 1 = 1
 -- AND fixture_id IN (
--- 1314297,
--- 1314298,
--- 1314299,
--- 1213882,
--- 1208599,
--- 1212922 -- Update this on daily basis
+-- 1345745,
+-- 1345747,
+-- 1345749,
+-- 1345751,
+-- 1208166 -- Update this on daily basis
 -- )
 -- ;
 
@@ -23,6 +22,7 @@ SELECT fixture_id
 FROM live_updates.live_fixtures
 WHERE 1 = 1
 AND status = 'NS'
+AND LENGTH(COALESCE(referee, 'aa')) > 3
 AND fixture_id IN (SELECT fixture_id FROM temp.raw_ffh WHERE player_pos IS NOT NULL)
 AND fixture_id IN (SELECT fixture_id FROM temp.important_fixtures)
 AND fixture_id NOT IN (SELECT fixture_id FROM temp.tele_fixtures WHERE 1 = 1)
